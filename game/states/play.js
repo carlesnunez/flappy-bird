143ff7860be44825a8029e8b9fdd98f501723cc7
Darkii <carles.nunez@hotmail.es>
2015-04-29 22:53:17 +0200

    (FEAT)Added all the logic of pipe spawn

--- game/states/play.js
+++ game/states/play.js
@@ -1,5 +1,6 @@
 var Bird = require('../prefabs/bird');
 var Ground = require('../prefabs/ground');
+var PipeGroup = require('../prefabs/pipeGroup');
 'use strict';
 function Play() {
 }
@@ -8,11 +9,15 @@ Play.prototype = {
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
     this.game.physics.arcade.gravity.y = 1200;
 
-    this.background = this.game.add.sprite(0,0, 'background');
-    this.bird = new Bird(this.game, 100, this.game.height/2);
+    this.background = this.game.add.sprite(0, 0, 'background');
+    this.bird = new Bird(this.game, 100, this.game.height / 2);
     this.ground = new Ground(this.game, 0, 400, 335, 112);
 
     this.game.add.existing(this.ground);
+    // create and add a group to hold our pipeGroup prefabs
+    this.pipes = this.game.add.group();
+
+
     this.game.add.existing(this.bird);
 
     /* previous setup code */
@@ -29,12 +34,25 @@ Play.prototype = {
     this.input.onDown.add(this.bird.flap, this.bird);
 
 
+    this.pipeGenerator = this.game.time.events.loop(Phaser.Timer.SECOND * 1.25, this.generatePipes, this);
+    this.pipeGenerator.timer.start();
+
   },
+
+  generatePipes: function() {
+    var pipeY = this.game.rnd.integerInRange(-100, 100);
+    var pipeGroup = this.pipes.getFirstExists(false);
+    if(!pipeGroup) {
+      pipeGroup = new PipeGroup(this.game, this.pipes);
+    }
+    pipeGroup.reset(this.game.width + pipeGroup.width/2, pipeY);
+  },
+
   update: function () {
     if (!!this.ready) {
       this.game.state.start('menu');
     }
-    if(this.game.physics.arcade.collide(this.bird, this.ground)){
+    if (this.game.physics.arcade.collide(this.bird, this.ground)) {
       //this.game.state.start('gameover');
     }
   },
--- game/states/preload.js
+++ game/states/preload.js
@@ -11,12 +11,13 @@ Preload.prototype = {
     this.asset.anchor.setTo(0.5, 0.5);
     this.load.setPreloadSprite(this.asset);
 
-    this.load.image('background','assets/background.png');
-    this.load.image('ground','assets/ground.png');
-    this.load.image('title','assets/title.png');
-    this.load.image('startButton','assets/start-button.png');
+    this.load.image('background', 'assets/background.png');
+    this.load.image('ground', 'assets/ground.png');
+    this.load.image('title', 'assets/title.png');
+    this.load.image('startButton', 'assets/start-button.png');
 
     this.load.spritesheet('bird', 'assets/pajaro.png', 34, 24, 3);
+    this.load.spritesheet('pipe', 'assets/pipes.png', 54, 320, 2);
   },
   create: function () {
     this.asset.cropEnabled = false;
